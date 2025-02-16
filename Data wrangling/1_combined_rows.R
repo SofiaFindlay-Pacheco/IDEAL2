94f6ae177cda31c58634adaa74d85e95077963f5
SofiaFindlay-Pacheco <s2736116@ed.ac.uk>
2025-02-16 21:38:53 +0000

    tidy up 1_combined_rows

--- Data wrangling/1_combined_rows.R
+++ Data wrangling/1_combined_rows.R
@@ -3,38 +3,36 @@ library(dplyr)
 library(tidyr)
 library(readxl)
 
+#Rearrange the miseq file into adequate form
+
 # Upload file path for the combined miseq results page
 file_path_combined <- "C:/Users/sofia/OneDrive - University of Edinburgh/master/R studio/IDEAL statistics/Edited original data/combined results page.xlsx"
 sheet_names <- excel_sheets(file_path_combined)
 
-# Read all sheets into a list
+# Read all miseq sheets into a vertical list
 sheets_list <- lapply(sheet_names, function(sheet) {read_excel(file_path_combined, sheet = sheet)})
 
-# Combine the sheets into one data frame, 
+# Turn combined sheets into one data frame
 combined_data <- bind_rows(sheets_list)
 
 
-# Combining AE and TB
-# Step 1: Filter for rows with `AE` or `TB`
+# Organize sampleID names into adequate form for analysis. By combining rows of same sampleID. 'AE' and 'TB' results from the same sample are now a single row for the whole sample.
+
+# Filter for rows with `AE` or `TB`
 data_ae <- combined_data %>% filter(grepl("AE", `Sample ID`))
 data_tb <- combined_data %>% filter(grepl("TB", "Sample ID"))
 
-# Step 2: Select specific columns: column 1, and columns 6 to 23
-data_ae <- data_ae %>% select("Sample ID", 1, 6:23, 25:31, 33)  # Select column 1 and columns 6 to 23
-data_tb <- data_tb %>% select("Sample ID", 1, 6:23, 25:31, 33)  # Select column 1 and columns 6 to 23
+# Select specific columns: column 1, and columns 6 to 23
+data_ae <- data_ae %>% select("Sample ID", 1, 6:23, 25:31, 33)  
+data_tb <- data_tb %>% select("Sample ID", 1, 6:23, 25:31, 33)  
 
-# Step 3: Remove the suffix "AE" or "TB" from the `Sample ID` to match them
+# Remove the suffix "AE" or "TB" from the SampleID
 data_ae <- data_ae %>% mutate(`Sample ID` = sub("AE$", "", `Sample ID`))  
 data_tb <- data_tb %>% mutate(`Sample ID` = sub("TB$", "", `Sample ID`))  
 
-# Step 4: Join the data frames by `Sample ID` to combine `AE` and `TB` data into one row per sample
-# add suffixes to distinguish AE and TB columns in the final data frame
+# Join the data frames by `SampleID` to combine `AE` and `TB` data into one row per sample
 combined_data <- full_join(data_ae, data_tb, by = "Sample ID", suffix = c("_AE", "_TB")) 
 
-# View the combined data
-head(combined_data)
-
+# Further refine combined data to only bacteria of interest, and check for NAs
 combined_data <- combined_data %>% select (1:11, 24, 38: 48, 52)
-
-
 combined_data[is.na(combined_data)] <- 0
