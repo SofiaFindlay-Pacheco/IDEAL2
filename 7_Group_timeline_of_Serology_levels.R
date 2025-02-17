d1cedb4ff6e54dfc67515cdd220ded7a6bb2b49e
SofiaFindlay-Pacheco <s2736116@ed.ac.uk>
2025-02-17 10:12:08 +0000

    tidy up 7

--- 7_Group_timeline_of_Serology_levels.R
+++ 7_Group_timeline_of_Serology_levels.R
@@ -1,23 +1,21 @@
-# Load necessary libraries
+# Libraries
 library(ggplot2)
 library(dplyr)
 library(lubridate)
 library(janitor)
+library(tidyr)
 
-
-# Convert dates to Date format
-#Brief_serology$`SampleWeek` <- as.Date(Brief_serology$`SampleWeek`)  # Converts to Date type
-
-# Handle missing values (if necessary)
+# Handle missing values
 Brief_serology_clean <- Brief_serology_clean %>% filter(!is.na(sample_week))
 
+# Convert columns to numbric format
 Brief_serology_clean$serology_t_parva <- as.numeric(Brief_serology_clean$serology_t_parva)
 Brief_serology_clean$serology_t_mutans <- as.numeric(Brief_serology_clean$serology_t_mutans)
 Brief_serology_clean$serology_b_bigemina <- as.numeric(Brief_serology_clean$serology_b_bigemina)
 Brief_serology_clean$serology_a_marginale <- as.numeric(Brief_serology_clean$serology_a_marginale)
 
-# Aggregate data by  visit date
-aggregated_data <- Brief_serology_clean %>%
+# Aggregate data by sample week to get overall bacterial levels each sample week overall for all calfs
+serology_by_Sample_week <- Brief_serology_clean %>%
   group_by(sample_week) %>%
   summarize(
     Avg_T_parva = mean(serology_t_parva, na.rm = TRUE),
@@ -26,20 +24,19 @@ aggregated_data <- Brief_serology_clean %>%
     Avg_A_marginale = mean(serology_a_marginale, na.rm = TRUE)
   )
 
-library(tidyr)
-
 # Reshape data to long format
-long_data <- aggregated_data %>%
+vertical_serology_by_Sample_week <- serology_by_Sample_week %>%
   pivot_longer(
     cols = starts_with("Avg_"),
     names_to = "Bacteria",
     values_to = "Average_Value"
   )
 
-long_data <- na.omit(long_data)
+# Remove NAs
+vertical_serology_by_Sample_week <- na.omit(vertical_serology_by_Sample_week)
 
 # Create the timeline plot
-ggplot(long_data, aes(x = sample_week, y = Average_Value, color = Bacteria, group = Bacteria)) +
+ggplot(vertical_serology_by_Sample_week, aes(x = sample_week, y = Average_Value, color = Bacteria, group = Bacteria)) +
   geom_line(linewidth = 1) +                      
   geom_point(size = 2) +                     
   scale_color_manual(values = c("red", "blue", "green", "purple")) +  
