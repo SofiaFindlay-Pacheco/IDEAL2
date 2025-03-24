962c62a6a1551025b9049cfb7d2a16ba951ffeb4
SofiaFindlay-Pacheco <s2736116@ed.ac.uk>
2025-03-24 08:09:05 +0000

    more edits

--- Data wrangling/data_wrangle_1-5.R
+++ Data wrangling/data_wrangle_1-5.R
@@ -2,8 +2,10 @@
 library(dplyr)
 library(tidyr)
 library(readxl)
+library(janitor)
+library(tidyverse)
 
-#Rearrange the miseq file into adequate form
+################### Rearrange the miseq data ############################
 
 # Upload file path for the combined miseq results page
 file_path_combined <- "C:/Users/sofia/OneDrive - University of Edinburgh/master/R studio/IDEAL statistics/Edited original data/combined results page.xlsx"
@@ -35,9 +37,8 @@ combined_data <- full_join(data_ae, data_tb, by = "Sample ID", suffix = c("_AE",
 # Further refine combined data to only bacteria of interest, and check for NAs
 combined_data <- combined_data %>% select (1:11, 24, 38: 48, 52)
 combined_data[is.na(combined_data)] <- 0
-# Libraries
-library(readxl)
-library(dplyr)
+
+########################### Combine Miseq data with Calf IDs ################################
 
 # Upload file path for the IDEAL sample database
 file_path_sample <- ("C:/Users/sofia/OneDrive - University of Edinburgh/master/R studio/IDEAL statistics/Edited original data/ideal_sample.xlsx")
@@ -55,13 +56,12 @@ calf_codes <- filtered_data %>%
 
 # View the result
 print(calf_codes)
+
 # Merge the calf codes dataframe to correspond with the miseq data frame based on Sample ID so that visit ID can be seen on miseq data
 merged_data <- merge(combined_data, calf_codes, by.x = "Sample ID", by.y = "codes", all = TRUE)
 merged_data <- merged_data[, c("VisitID", setdiff(names(merged_data), "VisitID"))] #rearranges so VisitID is first
-# Library 
-library(readxl)
-library(janitor)
 
+############################### Add important data from ideal calf data to database #############################
 
 # Upload the file path for the IDEAL calf data
 file_path <- "C:/Users/sofia/OneDrive - University of Edinburgh/master/R studio/IDEAL statistics/Edited original data/ideal_calf.xlsx"
@@ -77,7 +77,6 @@ ideal_calf$`Date last visit with data` <- as.numeric(ideal_calf$`Date last visit
 # Convert numeric Excel serial dates to Date format
 ideal_calf$`Date last visit with data` <- as.Date(ideal_calf$`Date last visit with data`, origin = "1899-12-30")
 
-
 # Combine merged_data with ideal_calf based on 'calfID' (merged_data) and 'visitID' (ideal_calf)
 final_miseq_data <- merge(merged_data, ideal_calf, by.x = "VisitID", by.y = "VisitID", all.x = TRUE, all.y = FALSE)
 #Clean data
@@ -90,47 +89,8 @@ final_miseq_data_clean <- final_miseq_data_clean %>%
 
 # Sample week based on first two numbers of VisitID
 final_miseq_data_clean$sample_week <- substr(final_miseq_data_clean$visit_id, 4, 5)
-#install.packages("tidyverse")
 
-# Remove rows where anaplasma_bovis_u03775_ae is NA
-final_miseq_data_clean <- final_miseq_data_clean %>%
-  filter(!is.na(anaplasma_bovis_u03775_ae))
-
-# Library
-library(tidyverse)
-library(readxl)
-library(janitor)
-
-
-# Upload the file path for the serology data
-file_path_serology <- "C:/Users/sofia/OneDrive - University of Edinburgh/master/R studio/IDEAL statistics/Edited original data/Serology_data.xlsx"
-serology <- read_excel(file_path_serology, sheet = "serology_data")
-# Clean up names
-serology_clean <- serology %>% clean_names()
-
-# Filter to rows where the contents of the "test" column begins with "Serology"
-filtered_serology <- serology_clean %>%
-  filter(grepl("Serology", test))
-
-#Filter to main columns of interest
-filtered_serology_main <- filtered_serology %>%
-  select(visit_id, visit_date, test, quantitative_result_number)
-
-#Flips data frame to be horizontal not vertical
-wide_serology <- filtered_serology_main %>%
-  pivot_wider(
-    names_from = test,  
-    values_from = quantitative_result_number  
-  )
-
-# Clean up names
-wide_serology_clean <- wide_serology %>% clean_names()
-
-# Fix problem of duplicate Visit date columns
-Serology_miseq_merge <- final_miseq_data_clean %>%
-  select(-visit_date) %>%  # Remove the duplicate column before joining
-  #merge miseq data and serology data
-  full_join(wide_serology_clean, by = "visit_id")
+################################ Add in date of death with postportem data #########################
 
 postmortem <- "C:/Users/sofia/OneDrive - University of Edinburgh/master/R studio/IDEAL statistics/Edited original data/ideal_postmortem.xlsx"
 ideal_postmortem <- read_excel(postmortem, sheet = "ideal_postmortem")
@@ -141,33 +101,10 @@ ideal_postmortem_clean$date_of_death <- as.Date(ideal_postmortem_clean$date_of_d
 final_miseq_data_clean <- final_miseq_data_clean %>%
   left_join(select(ideal_postmortem_clean, calf_id, date_of_death), by = "calf_id")
 
-#Sample week - changing all the dates to weeks of life
+################################## Sample week - changing all the dates to weeks of life ###############
 
 # Ensure all dates in date format
-Serology_miseq_merge[["visit_date"]] <- as.Date(Serology_miseq_merge[["visit_date"]], format = "%Y-%m-%d")
-Serology_miseq_merge[["date_of_birth"]] <- as.Date(Serology_miseq_merge[["date_of_birth"]], format = "%Y-%m-%d")
-
-# If sample week were to be calculated using equation
-#Serology_miseq_merge$Sample_Week <- ifelse(
-# !is.na(Serology_miseq_merge["visit_date"]) & !is.na(Serology_miseq_merge["date_of_birth"]), # Check for non-NA values
-#floor(as.numeric(difftime(Serology_miseq_merge$"visit_date", Serology_miseq_merge$"date_of_birth", units = "days")) / 7),
-#NA )# Set SampleWeek to NA if either date is missing)
-
-# Sample week based on first two numbers of VisitID
-Serology_miseq_merge$sample_week <- substr(Serology_miseq_merge$visit_id, 4, 5)
+final_miseq_data_clean[["visit_date"]] <- as.Date(final_miseq_data_clean[["visit_date"]], format = "%Y-%m-%d")
+final_miseq_data_clean[["date_of_birth"]] <- as.Date(final_miseq_data_clean[["date_of_birth"]], format = "%Y-%m-%d")
 
 
-
-# Select key columns of merged dataset, for just serology
-Brief_serology  <- Serology_miseq_merge %>% select (1,2,26:27,30,38:39,128:132,136 )
-
-# Sort the entire dataset based on the numeric part of CalfID
-Brief_serology  <- Brief_serology [order(as.numeric(gsub("[^0-9]", "", Brief_serology $calf_id))), ]
-
-# Clean up names
-Brief_serology_clean <- Brief_serology  %>% clean_names()
-
-
-#summary table
-summary_table <- Serology_miseq_merge %>%
-  select(visit_id, sample_week, visit_date, date_of_birth) 
